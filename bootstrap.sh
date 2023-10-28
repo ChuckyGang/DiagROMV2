0ae5a924e1ba09933c47a57c896f08f410d265e6
Erik Hemming <eriquesnk@gmail.com>
2023-10-28 21:50:24 +0200

    add bootstrap script for vasm/vbcc/vlink and ndk

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# source bootstrap.sh
+
+set -xe
+
+if [ ! -d vbcc ]; then
+  git clone https://github.com/erique/vbcc_vasm_vlink.git
+  cd vbcc_vasm_vlink && ./test.sh && cp -r build/vbcc .. && cd -
+  rm -rf vbcc_vasm_vlink
+fi
+
+if [ ! -d ndk ]; then
+  curl -#LO http://aminet.net/dev/misc/NDK3.2.lha
+  7z x NDK3.2.lha -ondk
+  rm NDK3.2.lha
+fi
+
+set +xe
+
+export VBCC=$PWD/vbcc
+export PATH=$PATH:$VBCC/bin
+export NDK=$PWD/ndk
